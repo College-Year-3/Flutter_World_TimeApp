3355ebeeb4656d6e4076cd321688694ebf782981
s00083446 <s00083446@mail.itsligo.ie>
2019-11-14 14:14:58 +0000

    added Stateful widgets to pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:world_time/pages/home.dart';
+//import 'pages/home.dart';
 
 void main() => runApp(MaterialApp(
-  home: 
+  home: Home(),
 ));
 
